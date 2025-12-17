3e53d7ea161aec663cc0f9556dcffd0856562b83
bida24-179 <bida24-179@thuto.bac.ac.bw>
2025-12-17 12:13:18 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,45 @@
+let cart = JSON.parse(localStorage.getItem('cart')) || [];
+
+
+function addToCart(name, price) {
+cart.push({ name, price });
+saveCart();
+renderCart();
+}
+
+
+function saveCart() {
+localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+
+function renderCart() {
+const cartItems = document.getElementById('cartItems');
+const totalEl = document.getElementById('total');
+if (!cartItems || !totalEl) return;
+
+
+cartItems.innerHTML = '';
+let total = 0;
+
+
+cart.forEach(item => {
+cartItems.innerHTML += `<p>${item.name} - P${item.price}</p>`;
+total += item.price;
+});
+
+
+totalEl.innerText = total;
+}
+
+
+function checkout() {
+if (!cart.length) return alert('Your cart is empty');
+alert('Order placed successfully. Phantom thanks you.');
+cart = [];
+saveCart();
+renderCart();
+}
+
+
+renderCart();
